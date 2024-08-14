9497eca5a2a52a99200b4daf247d98582cf7ec1f
puryigithan <puryigithan@gmail.com>
2024-08-15 00:42:09 +0300

    Dosyaları yükleme.
    Dosyalar yüklendi.

---
+++ Assets/Animation/Scaling.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaling : MonoBehaviour
+{
+    private Animator mAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+        mAnimator.SetTrigger("TrOpen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
